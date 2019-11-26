eac8f7ad81c9c00c480d18658c1c8f9f909e63c3
Simao Barroso <sbarroso@spartaglobal.com>
2019-11-26 10:32:58 +0000

    Added abstract person class

---
+++ src/main/java/com/sparta/e44/controllers/entities/AbstractPerson.java
@@ -0,0 +1,79 @@
+package com.sparta.e44.controllers.entities;
+
+import java.time.LocalDateTime;
+
+public abstract class AbstractPerson
+{
+    private int iD;
+    private String firstName;
+    private String lastname;
+    private LocalDateTime dateOfBirth;
+    private String email;
+    private String contactNumber;
+    private char gender;
+    private LocalDateTime startDate;
+
+    public int getiD() {
+        return iD;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDateTime getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDateTime dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+}
