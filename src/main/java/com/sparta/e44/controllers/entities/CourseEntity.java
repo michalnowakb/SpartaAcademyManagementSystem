74865a61cd15719b686888edcebaf2939d9b020b
Simao Barroso <sbarroso@spartaglobal.com>
2019-11-26 11:12:08 +0000

    Edited course entity

--- src/main/java/com/sparta/e44/controllers/entities/CourseEntity.java
+++ src/main/java/com/sparta/e44/controllers/entities/CourseEntity.java
@@ -1,4 +1,35 @@
 package com.sparta.e44.controllers.entities;
 
-public class CourseEntity {
+public class CourseEntity
+{
+//    private int/String courseId;
+    private String courseName;
+    private String courseDescription;
+    private String durationInWeeks;
+
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public String getDurationInWeeks() {
+        return durationInWeeks;
+    }
+
+    public void setDurationInWeeks(String durationInWeeks) {
+        this.durationInWeeks = durationInWeeks;
+    }
 }
