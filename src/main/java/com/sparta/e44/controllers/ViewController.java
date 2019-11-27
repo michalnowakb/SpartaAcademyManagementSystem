58fec004e3598186a18f3b76263e4a1edd922f4e
Jack McCabe <JMcCabe@spartaglobal.com>
2019-11-27 11:21:58 +0000

    update to controller

--- src/main/java/com/sparta/e44/controllers/ViewController.java
+++ src/main/java/com/sparta/e44/controllers/ViewController.java
@@ -1,15 +1,19 @@
 package com.sparta.e44.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ViewController {
 
     @RequestMapping("/trainerPage")
     public String connectToTrainerPage(){
         return "trainerPage";
+    }
+
+    @RequestMapping("/registerCoursePage")
+    public String connectToRegisterCoursePage(){
+        return "registerCoursePage";
     }
 }
