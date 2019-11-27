e607230753aa54e9bd12d725b9046e5227b79b0b
Jack McCabe <JMcCabe@spartaglobal.com>
2019-11-27 14:18:02 +0000

    updated view controller

--- src/main/java/com/sparta/e44/controllers/ViewController.java
+++ src/main/java/com/sparta/e44/controllers/ViewController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class ViewController {
 
+    //for the main page
     @RequestMapping("/trainerPage")
     public String connectToTrainerPage(){
         return "trainerPage";
@@ -16,16 +17,47 @@ public class ViewController {
     public String connectToCoursePage(){
         return "coursePage";
     }
-
     @RequestMapping("/traineePage")
     public String connectToTraineePage(){
         return "traineePage";
     }
+    //Course page
     @RequestMapping("/registerCoursePage")
     public String connectToRegisterCoursePage(){
         return "registerCoursePage";
     }
-
-
-
+    @RequestMapping("/viewCoursePage")
+    public String connectToViewCoursePage(){
+        return "viewCoursePage";
+    }
+    @RequestMapping("/updateCoursePage")
+    public String connectToUpdateCoursePage(){
+        return "updateCoursePage";
+    }
+    //Trainer Page
+    @RequestMapping("/registerTrainerPage")
+    public String connectToRegisterTrainerPage(){
+        return "registerTrainerPage";
+    }
+    @RequestMapping("/viewTrainerPage")
+    public String connectToViewTrainerPage(){
+        return "viewTrainerPage";
+    }
+    @RequestMapping("/updateTrainerPage")
+    public String connectToUpdateTrainerPage(){
+        return "updateTrainerPage";
+    }
+    // Trainee Page
+    @RequestMapping("/registerTraineePage")
+    public String connectToRegisterTraineePage(){
+        return "registerTraineePage";
+    }
+    @RequestMapping("/viewTraineePage")
+    public String connectToViewTraineePage(){
+        return "viewTraineePage";
+    }
+    @RequestMapping("/updateTraineePage")
+    public String connectToUpdateTraineePage(){
+        return "updateTraineePage";
+    }
 }
