edcc20e862fab85194f0bc6d496886e0e1c14d89
David Asare <gohan.david11@gmail.com>
2019-11-27 21:03:03 +0000

    Designed footer and added it onto every page

--- src/main/java/com/sparta/e44/controllers/ViewController.java
+++ src/main/java/com/sparta/e44/controllers/ViewController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
+    @RequestMapping("/indexPage")
+    public String viewIndexPage(){
+        return "indexPage";
+    }
+
     //for the main page
     @RequestMapping("/trainerPage")
     public String connectToTrainerPage(){
