7b5235c8eae28515348cdecb700e678146dbf894
James Mac <jamesmac9879@gmail.com>
2019-11-27 10:47:28 +0000

    Added css to register course page

--- src/main/java/com/sparta/e44/controllers/HomeController.java
+++ src/main/java/com/sparta/e44/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package com.sparta.e44.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String viewIndexPage(){
+        return "registerCoursePage";
+    }
 }
