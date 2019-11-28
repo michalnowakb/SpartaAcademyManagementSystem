1c7d304eafd6f5f21526bc5276d9181a6e0dfc48
Richard Ho <RHo@spartaglobal.com>
2019-11-28 13:36:05 +0000

    Trainee controller

--- src/main/java/com/sparta/e44/controllers/TraineeController.java
+++ src/main/java/com/sparta/e44/controllers/TraineeController.java
@@ -16,33 +16,33 @@ public class TraineeController {
     private TraineeService traineeService;
 
     @GetMapping("/trainee/getTrainees")
-    public String getTrainees(Model model){
+    public String getTrainees(Model model) {
 
-        model.addAttribute("trainees",traineeService.getAll());
+        model.addAttribute("trainees", traineeService.getAll());
         return "";
     }
 
     @GetMapping("/trainee/getTrainee/{id}")
-    public String getTrainee(@PathVariable("id") int id, Model model){
-        model.addAttribute("trainee",traineeService.getById(id));
+    public String getTrainee(@PathVariable("id") int id, Model model) {
+        model.addAttribute("trainee", traineeService.getById(id));
         return "";
     }
 
     @PostMapping("/trainee/addTrainee")
-    public String addTrainee(@Valid TraineeEntity newTrainee){
+    public String addTrainee(@Valid TraineeEntity newTrainee) {
         traineeService.addTrainee(newTrainee);
-        return "Added trainee";
+        return "";
     }
 
-    @PutMapping("/trainee/editTrainee/{id}")
-    public TraineeEntity editTrainee(@RequestBody TraineeEntity editedTrainee, @PathVariable("id") int id){
-        traineeService.editTrainee(editedTrainee,id);
-        return traineeService.getById(id);
+    @GetMapping("/trainee/editTrainee/{id}")
+    public String editTrainee(@RequestBody TraineeEntity editedTrainee, @PathVariable("id") int id) {
+        traineeService.editTrainee(editedTrainee, id);
+        return "";
     }
 
-    @DeleteMapping("/trainee/removeTrainee/{id}")
+    @GetMapping("/trainee/removeTrainee/{id}")
     public String removeCourse(@PathVariable("id") int id) {
         traineeService.removeTrainee(id);
-        return "Removed trainee";
+        return "";
     }
 }
