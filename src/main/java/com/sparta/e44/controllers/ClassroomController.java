c87ba36b7a848592c8c40a27f893678c02c0643d
Richard Ho <RHo@spartaglobal.com>
2019-11-29 15:53:32 +0000

    Classroom pages etc

--- src/main/java/com/sparta/e44/controllers/ClassroomController.java
+++ src/main/java/com/sparta/e44/controllers/ClassroomController.java
@@ -3,41 +3,50 @@ package com.sparta.e44.controllers;
 import com.sparta.e44.entities.ClassroomEntity;
 import com.sparta.e44.services.ClassroomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
-@RestController
+
+@Controller
 public class ClassroomController {
 
-     @Autowired
-     private ClassroomService classroomService;
+    @Autowired
+    private ClassroomService classroomService;
 
-     @GetMapping("/classroom/getClassrooms")
-    public List<ClassroomEntity> getClassrooms(){
-         return classroomService.getAll();
-     }
+
+    @GetMapping("/classroom/getClassrooms")
+    public String getClassrooms(Model model) {
+        model.addAttribute("classrooms", classroomService.getAll());
+        return "viewClassroomPage";
+    }
 
     @GetMapping("/classroom/getClassroom/{id}")
-    public ClassroomEntity getClassroom(@PathVariable int id){
-         return classroomService.getById(id);
-     }
+    public String getClassroom(@PathVariable int id, Model model) {
+        model.addAttribute("classroom", classroomService.getById(id));
+        return "updateClassroomPage";
+    }
 
-     @PostMapping("/classroom/addClassroom")
-     public String addClassroom(@RequestBody ClassroomEntity newClassroom){
-         classroomService.addClassroom(newClassroom);
-         return  "Added classroom";
-     }
 
-     @PutMapping("/classroom/editClassroom/{id}")
-    public ClassroomEntity editClassroom(@RequestBody ClassroomEntity editedClassroom, @PathVariable int id){
-         classroomService.editClassroom(editedClassroom,id);
-         return classroomService.getById(id);
-     }
+    @PostMapping("/classroom/addClassroom")
+    public String addClassroom(@Valid ClassroomEntity newClassroom) {
+        classroomService.addClassroom(newClassroom);
+        return "registerClassroomPage";
+    }
 
-     @DeleteMapping("/classroom/removeClassroom/{id}")
-    public  String removeClassroom(@PathVariable int id){
-         classroomService.removeClassroom(id);
-         return "Removed classroom";
-     }
+    @GetMapping("/classroom/editClassroom/{id}")
+    public String editClassroom(@Valid ClassroomEntity editedClassroom, @PathVariable int id, Model model) {
+        classroomService.editClassroom(editedClassroom, id);
+        model.addAttribute("classrooms", classroomService.getAll());
+        return "viewClassroomPage";
+    }
+
+    @GetMapping("/classroom/removeClassroom/{id}")
+    public String removeClassroom(@PathVariable int id, Model model) {
+        classroomService.removeClassroom(id);
+        model.addAttribute("classrooms", classroomService.getAll());
+        return "viewClassroomPage";
+    }
 }
