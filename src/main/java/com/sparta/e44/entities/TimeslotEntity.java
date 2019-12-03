6d3160908fbfff2004f22c197866b9b0d666c378
James Mac <jamesmac9879@gmail.com>
2019-12-03 11:17:38 +0000

    Edited timeslot

--- src/main/java/com/sparta/e44/entities/TimeslotEntity.java
+++ src/main/java/com/sparta/e44/entities/TimeslotEntity.java
@@ -10,12 +10,10 @@ public class TimeslotEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int timeslotId;
 
-    private String title;
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public TimeslotEntity(String title, LocalDate startDate, LocalDate endDate) {
-        this.title = title;
+    public TimeslotEntity(LocalDate startDate, LocalDate endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
     }
