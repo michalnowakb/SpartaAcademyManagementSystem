caead5f510b81cb4f21576ff945b3b14ade4b5f7
Joshua Steele-Gomes <JSteele-Gomes@spartaglobal.com>
2019-12-03 11:28:59 +0000

    added ENUM for classrooms to be selected in viewScheduleClassroom Form

--- src/main/java/com/sparta/e44/entities/state/Classrooms.java
+++ src/main/java/com/sparta/e44/entities/state/Classrooms.java
@@ -1,18 +1,44 @@
 package com.sparta.e44.entities.state;
 
-public enum Classes {
+public enum Classrooms {
 
-    Innovation,
-    Flexibility,
-    Empathy,
-    Drive,
-    Diversity,
-    Collaboration,
-    Turing,
+    Innovation("Innovation"),
+    Flexibility("Flexibility"),
+    Empathy("Empathy"),
+    Drive("Drive"),
+    Diversity("Diversity"),
+    Collaboration("Collaboration"),
+    Turing("Turing"),
+    ClassroomNotAvailable("Classroom Not Available");
 
-    public void getEnum(){
-        switch(classChosen){
-            case : Innovation
+
+    private String value;
+    Classrooms(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static Classrooms getClassroom(String classroom){
+        switch (classroom){
+            case "Innovation":
+                return Classrooms.Innovation;
+            case "Flexibility":
+                return Classrooms.Flexibility;
+            case "Empathy":
+                return Classrooms.Empathy;
+            case "Drive":
+                return Classrooms.Drive;
+            case "Diversity":
+                return Classrooms.Diversity;
+            case "Collaboration":
+                return Classrooms.Collaboration;
+            case "Turing":
+                return Classrooms.Turing;
         }
+        return Classrooms.ClassroomNotAvailable;
+
     }
 }
