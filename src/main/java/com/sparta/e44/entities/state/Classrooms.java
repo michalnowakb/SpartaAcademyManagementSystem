3c8f804a3bbcada04b2fe1c3da105a239be5fe55
Joshua Steele-Gomes <JSteele-Gomes@spartaglobal.com>
2019-12-03 11:27:31 +0000

    including name in fields to be referenced

---
+++ src/main/java/com/sparta/e44/entities/state/Classrooms.java
@@ -0,0 +1,18 @@
+package com.sparta.e44.entities.state;
+
+public enum Classes {
+
+    Innovation,
+    Flexibility,
+    Empathy,
+    Drive,
+    Diversity,
+    Collaboration,
+    Turing,
+
+    public void getEnum(){
+        switch(classChosen){
+            case : Innovation
+        }
+    }
+}
