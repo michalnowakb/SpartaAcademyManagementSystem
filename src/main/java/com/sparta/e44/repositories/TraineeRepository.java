73b30314d83545496863e7f012720e96f2c2304e
Daniel Curtis <DCurtis@spartaglobal.com>
2019-12-03 09:40:12 +0000

    Fixed back to previous to before last fix commit

--- src/main/java/com/sparta/e44/repositories/TraineeRepository.java
+++ src/main/java/com/sparta/e44/repositories/TraineeRepository.java
@@ -1,9 +1,17 @@
 package com.sparta.e44.repositories;
 
 import com.sparta.e44.entities.TraineeEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TraineeRepository extends CrudRepository<TraineeEntity, Integer> {
+
+    @Query("SELECT a FROM TraineeEntity a WHERE CONCAT(a.firstName,' ',a.lastName) LIKE CONCAT('%',:name,'%')")
+    public List<TraineeEntity> getTraineeEntitiesByName(@Param("name") String name);
+
 }
