f2819033ba9357c6db71d5bf812b484174d1cbe7
Joshua Steele-Gomes <JSteele-Gomes@spartaglobal.com>
2019-12-05 10:05:04 +0000

    Pushing file only to work on same file with Horia methods added to attempt reading from database

--- src/main/java/com/sparta/e44/controllers/TimeslotController.java
+++ src/main/java/com/sparta/e44/controllers/TimeslotController.java
@@ -1,6 +1,9 @@
 package com.sparta.e44.controllers;
 
+import com.sparta.e44.entities.ClassroomEntity;
+import com.sparta.e44.entities.TeachingGroupEntity;
 import com.sparta.e44.entities.TimeslotEntity;
+import com.sparta.e44.entities.TraineeEntity;
 import com.sparta.e44.services.ClassroomService;
 import com.sparta.e44.services.TeachingGroupService;
 import com.sparta.e44.services.TimeslotService;
@@ -10,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class TimeslotController {
@@ -22,6 +26,10 @@ public class TimeslotController {
 
     @Autowired
     private TeachingGroupService teachingGroupService;
+
+    private TeachingGroupEntity teachingGroupEntity;
+
+    private ClassroomEntity classroomEntity;
 
     @GetMapping("/schedule/getAllTimeslots")
     public String getAllTimeslots(Model model) {
@@ -50,10 +58,22 @@ public class TimeslotController {
 
     @GetMapping("/schedule/addTimeslotPage")
     public String addTimeSlotPage(Model model) {
-        model.addAttribute("classes", classroomService.getAll());
-        model.addAttribute("groups", teachingGroupService.getAllTeachingGroups());
+        int capacity =0;
+        List<TraineeEntity> numberOfTrainers = teachingGroupEntity.getTrainees();
+        for (TraineeEntity trainer : numberOfTrainers) {
+            capacity+=1;
+        }
+        if (capacity <= searchForCapacityOfId(classroomEntity.getClassroomName())){
+            model.addAttribute("classes", classroomService.getAll());
+            model.addAttribute("groups", teachingGroupService.getAllTeachingGroups());
+        }
+
         return "registerTimeslotPage";
     }
 
+    public int searchForCapacityOfId(String classroomName){
+        int capacity =0;
+        return capacity;
+    }
 
 }
